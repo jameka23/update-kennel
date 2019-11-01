34b0b779f80f213703050e8fbb0b0f72f9580cd7
Jameka Echols <jeechols@aosmith.com>
2019-11-01 07:59:04 -0500

    added employee component

---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import {
+    Card, CardText, CardBody,
+    CardTitle
+} from 'reactstrap'
+import '../Kennel.css'
+
+export default class EmployeeCard extends Component {
+    render(){
+        return(
+            <section>
+                <Card>
+                    <CardBody>
+                        <CardTitle></CardTitle>
+                    </CardBody>
+                </Card>
+            </section>
+        )
+    }
+}
